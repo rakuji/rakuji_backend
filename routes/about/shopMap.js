622dc59ca56a6b2ef8ea9755698c5cc93fd9300d
rakuji <rakuji.github@gmail.com>
2022-06-12 00:59:28 +0800

    shopmap 0612

--- routes/about/shopMap.js
+++ routes/about/shopMap.js
@@ -3,13 +3,19 @@ var router = express.Router();
 const connection = require('../../modules/mysql_config');
 
 ///http://localhost:3001/shopmap/area
-router.get('/area',(req,res,next)=>{
+// router.get('/area',(req,res,next)=>{
+//     const sql = "select * from shop_area order by area_id desc";   
+//     connection.query(sql,function(err,results,fields){
+//         console.log(err)
+//        res.json(results);
+//     })
+// })
+router.get('/area',async (req,res,next)=>{
     const sql = "select * from shop_area order by area_id desc";   
-    connection.query(sql,function(err,results,fields){
-        console.log(err)
-       res.json(results);
+    const [results] = await connection.query(sql);
+    res.json(results);
     })
-})
+
 //http://localhost:3001/shopmap/shop?area_id=2
 router.get('/shop',(req,res,next)=>{
     const sql = "select shop_id, shop_name from shop_info where area_id=? order by shop_id";   
